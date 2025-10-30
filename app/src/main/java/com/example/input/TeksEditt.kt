62541b396827d1fbf463a714cb473601891a0fbf
verss21 <arjunafrizal01@gmail.com>
2025-10-30 16:24:04 +0700

    menambahkan komponen HorizontalDivider sebagai pemisah antarbagian form data diri

--- app/src/main/java/com/example/input/TeksEditt.kt
+++ app/src/main/java/com/example/input/TeksEditt.kt
@@ -84,5 +84,15 @@ fun FormDataDiri(modifier: Modifier
             }
         )
 
+        HorizontalDivider(
+            modifier = Modifier.padding(
+                bottom = dimensionResource(R.dimen.padding_medium),
+                top = dimensionResource(
+                    id = R.dimen.padding_medium
+                )
+            ),
+            thickness = dimensionResource(R.dimen.divider_tipis),
+            color = Color.DarkGray
+        )
 
 
