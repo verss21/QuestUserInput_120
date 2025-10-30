2240666d4e816b74d2fa1b6cb7f1ec8d2a0db40a
verss21 <arjunafrizal01@gmail.com>
2025-10-30 16:28:01 +0700

    menampilkan data hasil input (Nama, Gender, Alamat) di dalam ElevatedCard

--- app/src/main/java/com/example/input/TeksEditt.kt
+++ app/src/main/java/com/example/input/TeksEditt.kt
@@ -125,4 +125,15 @@ fun FormDataDiri(modifier: Modifier
             modifier = Modifier
                 .height(100.dp)
                 .width(300.dp)
-        )
+        ) {
+            Column(
+                modifier = Modifier.padding(horizontal = 5.dp, vertical = 15.dp)
+            ) {
+                Text(text = "Nama: "+nama, color = Color.White)
+                Text(text = "Gender: "+jenis, color = Color.White)
+                Text(text = "Alamat: "+alamat, color = Color.White)
+            }
+        }
+    }
+}
+
