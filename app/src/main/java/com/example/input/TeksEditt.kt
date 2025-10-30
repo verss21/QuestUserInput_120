712aeafe4dbb793f1d7ae06695b1c7f0c1569f81
verss21 <arjunafrizal01@gmail.com>
2025-10-30 16:27:20 +0700

    menambahkan komponen ElevatedCard dengan warna hitam dan elevasi 10dp pada form data diri

--- app/src/main/java/com/example/input/TeksEditt.kt
+++ app/src/main/java/com/example/input/TeksEditt.kt
@@ -118,3 +118,11 @@ fun FormDataDiri(modifier: Modifier
             thickness = dimensionResource(R.dimen.divider_tipis),
             color = Color.DarkGray
         )
+
+        ElevatedCard(
+            elevation = CardDefaults.cardElevation(defaultElevation = 10.dp),
+            colors = CardDefaults.cardColors(containerColor = Color.Black),
+            modifier = Modifier
+                .height(100.dp)
+                .width(300.dp)
+        )
